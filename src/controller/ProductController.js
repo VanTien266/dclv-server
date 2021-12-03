cc978dc17a8a1b66f6206c9f52dc26958073224a
Luu Van Tien <luuvantien2006@gmail.com>
2021-12-04 00:19:17 +0700

    add api about product

--- src/controller/ProductController.js
+++ src/controller/ProductController.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const router = express.Router();
 
 const { FabricRoll } = require("../models/FabricRoll");
+const { Item } = require("../models/Item");
+const { MarketPrice } = require("../models/MarketPrice");
 
 //Get all product in db
 const getProductList = async (req, res) => {
@@ -16,6 +18,7 @@ const getProductList = async (req, res) => {
           let: { color_code: "$colorCode" },
           pipeline: [
             { $match: { $expr: { $eq: ["$colorCode", "$$color_code"] } } },
+            { $unwind: { path: "$marketPriceId" } }, //split array
             {
               $lookup: {
                 from: "MarketPrice",
@@ -42,6 +45,15 @@ const getProductList = async (req, res) => {
               },
             },
             { $unwind: "$fabricType" },
+            {
+              $group: {
+                _id: "$_id",
+                colorCode: { $first: "$colorCode" },
+                name: { $first: "$name" },
+                marketPrice: { $push: "$marketPrice" },
+                fabricType: { $first: "$fabricType" },
+              },
+            },
           ],
           as: "item",
         },
@@ -62,10 +74,10 @@ const getProductById = async (req, res) => {
       {
         $lookup: {
           from: "Item",
-          let: { color_code: "$colorCode" }, //assign colorCode in FabricRoll to color_code var
+          let: { color_code: "$colorCode" },
           pipeline: [
-            { $match: { $expr: { $eq: ["$colorCode", "$$color_code"] } } }, //colorCode in Item===color_code in FabricRoll
-            { $unwind: "$marketPriceId" },
+            { $match: { $expr: { $eq: ["$colorCode", "$$color_code"] } } },
+            { $unwind: { path: "$marketPriceId" } },
             {
               $lookup: {
                 from: "MarketPrice",
@@ -74,19 +86,13 @@ const getProductById = async (req, res) => {
                   {
                     $match: { $expr: { $eq: ["$_id", "$$market_price_id"] } },
                   },
-                  {
-                    $group: {
-                      _id: "$_id",
-                      marketPriceId: { $push: "$marketPriceId" },
-                    },
-                  },
                 ],
                 as: "marketPrice",
               },
             },
 
-            // { $group: { _id: "$marketPriceId" } },
             { $unwind: "$marketPrice" },
+
             {
               $lookup: {
                 from: "FabricType",
@@ -100,11 +106,20 @@ const getProductById = async (req, res) => {
               },
             },
             { $unwind: "$fabricType" },
+            {
+              $group: {
+                _id: "$_id",
+                colorCode: { $first: "$colorCode" },
+                name: { $first: "$name" },
+                marketPrice: { $push: "$marketPrice" },
+                fabricType: { $first: "$fabricType" },
+              },
+            },
           ],
-          as: "colorCode",
+          as: "item",
         },
       },
-      { $unwind: "$colorCode" },
+      { $unwind: "$item" },
     ]);
     res.status(200).json(product);
   } catch (err) {
@@ -119,21 +134,46 @@ const updateProductStatus = async (req, res) => {
     const id = mongoose.Types.ObjectId(req.params.id);
 
     FabricRoll.findOneAndUpdate({ _id: id }, body, function (err, data) {
-      if (!err) res.status(200).json("Update successfully!");
+      if (!err) res.status(200).json("Update Status successfully!");
     });
   } catch (err) {
     res.status(500).json({ err });
   }
 };
 
-//Update market price
-// const updateMarketPrice = async (req, res) => {
-//   try {
-//     const body = req.body;
-//     const id = mongoose.params.id;
+// Update market price
+const updateMarketPrice = async (req, res) => {
+  try {
+    const body = req.body;
+    const id = req.params.id;
+    console.log(id, body);
 
-//     Item;
-//   } catch (err) {}
-// };
+    const newMarketPrice = new MarketPrice({
+      dayApplied: Date.now(),
+      price: body.price,
+    });
 
-module.exports = { getProductList, getProductById, updateProductStatus };
+    newMarketPrice.save(function (err) {
+      if (!err) console.log(newMarketPrice);
+      else console.log(err);
+    });
+    Item.findByIdAndUpdate(
+      { _id: id },
+      {
+        $push: { marketPriceId: newMarketPrice._id },
+      },
+      function (err, data) {
+        if (!err) res.status(200).json("Update Market Price successfully!");
+      }
+    );
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+};
+
+module.exports = {
+  getProductList,
+  getProductById,
+  updateProductStatus,
+  updateMarketPrice,
+};
